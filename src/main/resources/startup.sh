e96953e017f2c3e9ddfe380b27c66cdbf7a9ed6d
d <d>
2017-08-07 10:21:33 +0800

    create sh

---
+++ src/main/resources/startup.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+#
+#
+APP_DIR=/home/developer/eju-micro-app
+APP_NAME=eju-micro-app
+APP_CONF=$APP_DIR/application.properties
+
+#set java home
+export JAVA_HOME=/opt/jdk1.8.0_111
+
+usage() {
+    echo "Usage: sh eju-micro-app.sh [start|stop|deploy]"
+    exit 1
+}
+
+kills(){
+    tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+    if [[ $tpid ]]; then
+        echo 'Kill Process!'
+        kill -9 $tpid
+    fi
+}
+
+start(){
+    rm -f $APP_DIR/tpid
+    #nohup java -jar myapp.jar --spring.config.location=application.yml > /dev/null 2>&1 &
+    nohup java -jar $APP_DIR/"$APP_NAME".jar > /dev/null 2>&1 &
+    echo $! > $APP_DIR/tpid
+    echo Start Success!
+}
+
+stop(){
+        tpid1=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+    echo tpid1-$tpid1
+        if [[ $tpid1 ]]; then
+        echo 'Stop Process...'
+        kill -15 $tpid1
+    fi
+    sleep 5
+    tpid2=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+        echo tpid2-$tpid2
+    if [[ $tpid2 ]]; then
+        echo 'Kill Process!'
+        kill -9 $tpid2
+    else
+        echo 'Stop Success!'
+    fi
+
+}
+
+check(){
+    tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+    if [[ tpid ]]; then
+        echo 'App is running.'
+    else
+        echo 'App is NOT running.'
+    fi
+}
+
+deploy() {
+    kills
+    rm -rf $APP_DIR/"$APPNAME".jar
+    cp "$APP_NAME".jar $APP_DIR
+}
+
+case "$1" in
+    "start")
+        start
+        ;;
+    "stop")
+        stop
+        ;;
+    "kill")
+        kills
+        ;;
+    "deploy")
+        deploy
+        ;;
+    *)
+        usage
+        ;;
+esac
