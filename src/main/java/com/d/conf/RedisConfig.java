3f667f1f534147edeae8a0e833cedb3cc8fc5cb9
d <d>
2018-05-09 09:30:06 +0800

    init

---
+++ src/main/java/com/d/conf/RedisConfig.java
@@ -0,0 +1,167 @@
+package com.d.conf;
+
+import java.io.Serializable;
+import java.lang.reflect.Method;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+//import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import redis.clients.jedis.JedisPoolConfig;
+
+//@EnableCaching
+//@SpringBootConfiguration
+public class RedisConfig extends CachingConfigurerSupport {
+
+	private final Logger logger = LoggerFactory.getLogger(RedisConfig.class);
+
+	@Value("${spring.redis.host}")
+	private String host;
+
+	@Value("${spring.redis.port}")
+	private int port;
+
+	@Value("${spring.redis.timeout}")
+	private int timeout;
+
+	@Value("${spring.redis.password}")
+	private String password;
+
+	@Value("${spring.redis.database}")
+	private int database;
+
+	@Value("${spring.redis.pool.max-idle}")
+	private int maxIdle;
+
+	@Value("${spring.redis.pool.min-idle}")
+	private int minIdle;
+
+	@Value("${spring.redis.expired}")
+	private long expired;
+	/**
+	 * 注解@Cache key生成规则
+	 */
+	@Bean
+	public KeyGenerator keyGenerator() {
+		return new KeyGenerator() {
+			@Override
+			public Object generate(Object target, Method method, Object... params) {
+				StringBuilder sb = new StringBuilder();
+				sb.append(target.getClass().getName());
+				sb.append(method.getName());
+				if (params != null && params.length > 0) {
+					for (Object obj : params) {
+						sb.append(obj.toString());
+					}
+				}
+				return sb.toString();
+			}
+		};
+	}
+
+	@Bean
+	public CacheManager cacheManager(RedisTemplate<?, ?> redisTemplate) {
+		RedisCacheManager redisCacheManager = new RedisCacheManager(redisTemplate);
+		// 设置缓存过期时间
+		redisCacheManager.setDefaultExpiration(expired);// 秒
+		return redisCacheManager;
+	}
+
+	@Bean
+	public RedisTemplate<Serializable, Serializable> redisTemplate(RedisConnectionFactory factory) {
+		RedisTemplate<Serializable, Serializable> redisTemplate = new RedisTemplate<Serializable, Serializable>();
+		// key序列化方式;（不然会出现乱码;）,但是如果方法上有Long等非String类型的话，会报类型转换错误；
+		// 所以在没有自己定义key生成策略的时候，以下这个代码建议不要这么写，可以不配置或者自己实现 ObjectRedisSerializer
+		// 或者JdkSerializationRedisSerializer序列化方式;
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+		redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
+		// 以上4条配置可以不用
+		redisTemplate.setConnectionFactory(factory);
+		return redisTemplate;
+	}
+
+	/**
+	 * redis连接的基础设置
+	 * 
+	 * @Description:
+	 * @return
+	 */
+	@Bean
+	public JedisConnectionFactory redisConnectionFactory() {
+		JedisConnectionFactory factory = new JedisConnectionFactory();
+		factory.setHostName(host);
+		factory.setPort(port);
+		factory.setPassword(password);
+		// 存储的库
+		factory.setDatabase(database);
+		// 设置连接超时时间
+		factory.setTimeout(timeout);
+		factory.setUsePool(true);
+		factory.setPoolConfig(jedisPoolConfig());
+		return factory;
+	}
+
+	/**
+	 * 连接池配置
+	 * 
+	 * @Description:
+	 * @return
+	 */
+	@Bean
+	public JedisPoolConfig jedisPoolConfig() {
+		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+		jedisPoolConfig.setMaxIdle(maxIdle);
+		jedisPoolConfig.setMinIdle(minIdle);
+		// jedisPoolConfig.set ...
+		return jedisPoolConfig;
+	}
+
+	/**
+	 * redis数据操作异常处理 这里的处理：在日志中打印出错误信息，但是放行
+	 * 保证redis服务器出现连接等问题的时候不影响程序的正常运行，使得能够出问题时不用缓存
+	 * 
+	 * @return
+	 */
+	@Bean
+	@Override
+	public CacheErrorHandler errorHandler() {
+		CacheErrorHandler cacheErrorHandler = new CacheErrorHandler() {
+			@Override
+			public void handleCacheGetError(RuntimeException e, Cache cache, Object key) {
+				logger.error("redis异常：key=[{}]", key, e);
+			}
+
+			@Override
+			public void handleCachePutError(RuntimeException e, Cache cache, Object key, Object value) {
+				logger.error("redis异常：key=[{}]", key, e);
+			}
+
+			@Override
+			public void handleCacheEvictError(RuntimeException e, Cache cache, Object key) {
+				logger.error("redis异常：key=[{}]", key, e);
+			}
+
+			@Override
+			public void handleCacheClearError(RuntimeException e, Cache cache) {
+				logger.error("redis异常：", e);
+			}
+		};
+		return cacheErrorHandler;
+	}
+
+}
