8b58395dd22edcc324e54544c779864be978e4f8
d <d>
2018-05-10 18:05:01 +0800

    valid aop

---
+++ src/main/java/com/d/aop/ValidAspect.java
@@ -0,0 +1,45 @@
+package com.d.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+
+@Component
+@Aspect
+public class ValidAspect {
+	@Pointcut(value = "execution(* com.d.web..*.*(..))")
+	public void access() {
+	}
+
+	@Around("access()")
+	public <T> Object around(ProceedingJoinPoint pjp) {
+		try {
+			for (Object a : pjp.getArgs()) {
+				if (a instanceof BindingResult) {
+					BindingResult r = (BindingResult) a;
+					if (r.getErrorCount() > 0) {
+						StringBuilder s = new StringBuilder();
+						for (ObjectError e : r.getAllErrors()) {
+							if (e instanceof FieldError) {
+								FieldError fe = (FieldError) e;
+								s.append(fe.getField()).append(fe.getDefaultMessage()).append(";");
+							} else {
+								s.append(e.getCode()).append(e.getDefaultMessage()).append(";");
+							}
+						}
+						return s.toString();
+					}
+				}
+			}
+			return pjp.proceed();
+		} catch (Throwable throwable) {
+			throwable.printStackTrace();
+			return null;
+		}
+	}
+}
