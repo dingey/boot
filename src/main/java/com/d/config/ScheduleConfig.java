87ad65e482da507f3ba8355fefb9b5834a414a4b
d <d>
2018-05-24 22:57:21 +0800

    profile

--- src/main/java/com/d/config/ScheduleConfig.java
+++ src/main/java/com/d/config/ScheduleConfig.java
@@ -7,13 +7,15 @@ import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 @Configuration
+@Profile("prod")
 public class ScheduleConfig implements SchedulingConfigurer {
 	Logger logger = LoggerFactory.getLogger(SchedulingConfigurer.class);
-	int size = 1;// Runtime.getRuntime().availableProcessors();
+	int size = Runtime.getRuntime().availableProcessors();
 
 	@PostConstruct
 	public void initMethod() {
