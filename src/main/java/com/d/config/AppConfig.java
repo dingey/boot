486fc150b34720ce142a673cdf1f443f1bfea4b1
d <d>
2018-06-22 21:14:55 +0800

    edit

---
+++ src/main/java/com/d/config/AppConfig.java
@@ -0,0 +1,19 @@
+package com.d.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("dev")
+@Configuration
+public class AppConfig {
+    static Logger logger = LoggerFactory.getLogger(AppConfig.class);
+
+    static {
+        logger.info("配置应用dev环境开始。。。");
+        if (System.getenv("db_pwd") == null && System.getProperty("db_pwd") == null) {
+            System.setProperty("db_pwd", "root");
+        }
+    }
+}
