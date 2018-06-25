89cfaaa593f02354438af4067d3f2de2228a7d72
d <d>
2018-06-25 21:57:05 +0800

    batch

--- src/main/java/com/d/mapper/UserMapper.java
+++ src/main/java/com/d/mapper/UserMapper.java
@@ -7,18 +7,26 @@ import com.di.kit.SqlProvider;
 
 import org.apache.ibatis.annotations.*;
 
+import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 public interface UserMapper extends BaseMapper<User> {
 
-	List<User> getAll();
+    List<User> getAll();
 
-	@SelectProvider(type = SqlProvider.class, method = "get")
-	@Results({ @Result(property = "userSex", column = "user_sex", javaType = UserSexEnum.class) })
-	User getOne(User u);
-	
-	User getById(long id);
-	
-	@Select("select * from user where username=#{username}")
-	User getByUsername(String username);
+    @SelectProvider(type = SqlProvider.class, method = "get")
+    @Results({@Result(property = "userSex", column = "user_sex", javaType = UserSexEnum.class)})
+    User getOne(User u);
+
+    User getById(long id);
+
+    @Select("select * from user where username=#{username}")
+    User getByUsername(String username);
+
+    List<User> query(@Param("id") Integer id, @Param("ids") Collection<Integer> ids, @Param("start") Date start, @Param("end") Date end);
+
+    int batchInsert(List<User> users);
+
+    int batchUpdate(List<User> users);
 }
