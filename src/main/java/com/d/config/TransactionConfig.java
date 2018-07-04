3a626fdbd0f765f12a9f0fb5e7edf9a26565a28d
dingey <123929187@qq.com>
2018-07-04 17:38:50 +0800

    Create TransactionConfig.java

---
+++ src/main/java/com/d/config/TransactionConfig.java
@@ -0,0 +1,43 @@
+
+import org.springframework.aop.framework.autoproxy.BeanNameAutoProxyCreator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
+
+import java.util.Properties;
+
+/**
+ * @auther d
+ */
+@Configuration
+public class TransactionConfig {
+    private final DataSourceTransactionManager transactionManager;
+
+    @Autowired
+    public TransactionConfig(DataSourceTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    @Bean(name = "txAdvice")
+    public TransactionInterceptor getAdvisor() {
+        Properties properties = new Properties();
+        properties.setProperty("get*", "PROPAGATION_REQUIRED,-Exception");
+        properties.setProperty("add*", "PROPAGATION_REQUIRED,-Exception");
+        properties.setProperty("save*", "PROPAGATION_REQUIRED,-Exception");
+        properties.setProperty("update*", "PROPAGATION_REQUIRED,-Exception");
+        properties.setProperty("delete*", "PROPAGATION_REQUIRED,-Exception");
+        properties.setProperty("*", "PROPAGATION_REQUIRED,-Exception");
+        return new TransactionInterceptor(transactionManager, properties);
+    }
+
+    @Bean
+    public BeanNameAutoProxyCreator txProxy() {
+        BeanNameAutoProxyCreator creator = new BeanNameAutoProxyCreator();
+        creator.setInterceptorNames("txAdvice");
+        creator.setBeanNames("*Service", "*ServiceImpl");
+        creator.setProxyTargetClass(true);
+        return creator;
+    }
+}
