f0831e2263f1273da69d19b9bf8445dabc3736bf
dingey <123929187@qq.com>
2018-07-05 22:55:32 +0800

    Update WebMvcConfig.java

--- src/main/java/com/d/config/WebMvcConfig.java
+++ src/main/java/com/d/config/WebMvcConfig.java
@@ -1,19 +1,13 @@
 package com.d.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class WebMvcConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
-            }
-        };
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowCredentials(true).allowedMethods("GET", "POST").maxAge(3600);
     }
 }
