45474e935c0ddb7c0903183b986a6febd661ec4a
d <d>
2018-07-07 15:39:13 +0800

    format

--- src/main/java/com/d/base/BaseMapper.java
+++ src/main/java/com/d/base/BaseMapper.java
@@ -14,53 +14,53 @@ import org.apache.ibatis.jdbc.SQL;
  */
 @SuppressWarnings("unused")
 public interface BaseMapper<T> {
-	@InsertProvider(type = SqlProvider.class, method = "insert")
-	@Options(useGeneratedKeys = true)
-	int insert(T t);
+    @InsertProvider(type = SqlProvider.class, method = "insert")
+    @Options(useGeneratedKeys = true)
+    int insert(T t);
 
-	@InsertProvider(type = SqlProvider.class, method = "insertSelective")
-	@Options(useGeneratedKeys = true)
-	int insertSelective(T t);
+    @InsertProvider(type = SqlProvider.class, method = "insertSelective")
+    @Options(useGeneratedKeys = true)
+    int insertSelective(T t);
 
-	@UpdateProvider(type = SqlProvider.class, method = "update")
-	int update(T t);
+    @UpdateProvider(type = SqlProvider.class, method = "update")
+    int update(T t);
 
-	@UpdateProvider(type = SqlProvider.class, method = "updateSelective")
-	int updateSelective(T t);
+    @UpdateProvider(type = SqlProvider.class, method = "updateSelective")
+    int updateSelective(T t);
 
-	@UpdateProvider(type = SqlProvider.class, method = "deleteMark")
-	int delete(T t);
+    @UpdateProvider(type = SqlProvider.class, method = "deleteMark")
+    int delete(T t);
 
-	@SelectProvider(type = SqlProvider.class, method = "get")
-	T get(T t);
+    @SelectProvider(type = SqlProvider.class, method = "get")
+    T get(T t);
 
-	@SelectProvider(type = SqlProvider.class, method = "getById")
-	T getById(Class<T> t, Serializable id);
+    @SelectProvider(type = SqlProvider.class, method = "getById")
+    T getById(Class<T> t, Serializable id);
 
-	@SelectProvider(type = SqlProvider.class, method = "list")
-	List<T> list(T t);
+    @SelectProvider(type = SqlProvider.class, method = "list")
+    List<T> list(T t);
 
-	@SelectProvider(type = SqlProvider.class, method = "count")
-	Integer count(T t);
+    @SelectProvider(type = SqlProvider.class, method = "count")
+    Integer count(T t);
 
-	@SelectProvider(type = SqlProvider.class, method = "listAll")
-	List<T> listAll(Class<T> t);
+    @SelectProvider(type = SqlProvider.class, method = "listAll")
+    List<T> listAll(Class<T> t);
 
-	@SelectProvider(type = SqlProvider.class, method = "countAll")
-	int countAll(Class<T> t);
+    @SelectProvider(type = SqlProvider.class, method = "countAll")
+    int countAll(Class<T> t);
 
-	@SelectProvider(type = SqlProvider.class, method = "listByIds")
-	List<T> listByIds(Class<T> t, Iterable<Integer> ids);
+    @SelectProvider(type = SqlProvider.class, method = "listByIds")
+    List<T> listByIds(Class<T> t, Iterable<Integer> ids);
 
-    	@Deprecated
-   	 @Select("${sql.toString()}")
-    	LinkedHashMap getBySQL(@Param("sql") SQL sql);
+    @Select("${sql.toString()}")
+    @Deprecated
+    LinkedHashMap getBySQL(@Param("sql") SQL sql);
 
-	@Deprecated
-	@Select("${sql.toString()}")
-	List<LinkedHashMap> listBySQL(@Param("sql") SQL sql);
+    @Select("${sql.toString()}")
+    @Deprecated
+    List<LinkedHashMap> listBySQL(@Param("sql") SQL sql);
 
-   	@Deprecated
-    	@Select("${sql.toString()}")
-    	Integer countBySQL(@Param("sql") SQL sql);
+    @Select("${sql.toString()}")
+    @Deprecated
+    Integer countBySQL(@Param("sql") SQL sql);
 }
